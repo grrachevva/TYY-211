1f0f0d9e341a3ad8e6328189fdfb9dbac500fabc
FlashAnton <safronov-ai@mail.ru>
2024-11-23 23:42:31 +0300

    Пример работы с таблицами Word

---
+++ testWordTable/testWordTable/frmMain.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace testWordTable
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            this.dgMain = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(12, 12);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Создать...";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(93, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Открыть...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileOk += new System.ComponentModel.CancelEventHandler(this.dlgOpen_FileOk);
+            // 
+            // dgMain
+            // 
+            this.dgMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMain.Location = new System.Drawing.Point(12, 41);
+            this.dgMain.Name = "dgMain";
+            this.dgMain.RowHeadersVisible = false;
+            this.dgMain.Size = new System.Drawing.Size(914, 223);
+            this.dgMain.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 281);
+            this.Controls.Add(this.dgMain);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.btnCreate);
+            this.Name = "frmMain";
+            this.Text = "Главная экранная форма (Word)";
+            ((System.ComponentModel.ISupportInitialize)(this.dgMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+        private System.Windows.Forms.DataGridView dgMain;
+    }
+}
+
---
+++ testWordTable/testWordTable/frmMain.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using W = Microsoft.Office.Interop.Word;
+
+namespace testWordTable
+{
+    public partial class frmMain : Form
+    {
+        public frmMain() => InitializeComponent();
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            dlgOpen.InitialDirectory = Application.StartupPath;
+            dlgOpen.Filter = "MS Word|*.docx|Все|*.*";
+            dlgOpen.Title = "Выберите список трудов...";
+            dlgOpen.ShowDialog();
+        }
+
+        private void dlgOpen_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string path;
+
+            if (!e.Cancel)
+            {
+                path = (sender as OpenFileDialog).FileName;
+                wordCore(path);
+            }
+        }
+
+        private void wordCore(string path)
+        {
+            W.Application oWord;
+            W.Document oDoc;
+            int counter = 0;
+
+            oWord = new W.Application();
+            oDoc = oWord.Documents.Open(path);
+
+            foreach (W.Table oTab in oDoc.Tables)
+            {
+                if (oTab.Columns.Count == 6)
+                {
+                    if (counter == 0)
+                    {
+                        dgMain.Rows.Clear();
+                        dgMain.Columns.Clear();
+                        for (int i = 0; i < oTab.Columns.Count; i++)
+                            dgMain.Columns.Add("col" + (i + 1),
+                                oTab.Cell(1, i + 1).Range.Text.TrimEnd(new char[] { '\r', '\a' }));
+                    }
+                    dgMain.Rows.Add(oTab.Rows.Count - 1);
+                    for (int i = 1; i < oTab.Rows.Count; i++)
+                        for (int j = 0; j < oTab.Columns.Count; j++)
+                        {
+                            if (!dgMain.Columns[j].HeaderText.Equals(
+                                oTab.Cell(i + 1, j + 1).Range.Text.TrimEnd(new char[] { '\r', '\a' })))
+                            {
+                                dgMain[j, counter + i - 1].Value =
+                                    oTab.Cell(i + 1, j + 1).Range.Text.TrimEnd(new char[] { '\r', '\a' });
+                            }
+                            else
+                            {
+                                counter--;
+                                break;
+                            }
+                        }
+                    counter += oTab.Rows.Count - 1;
+                }
+            }
+            oWord.Quit();
+        }
+
+        private void CustomizedParagraph(ref W.Paragraph fPr, 
+            string fText, int fBold = 0,
+            W.WdColor fColor = W.WdColor.wdColorBlack, 
+            string fFntName = "Times New Roman", 
+            W.WdParagraphAlignment fAlign = 
+                W.WdParagraphAlignment.wdAlignParagraphJustify)
+        {
+            fPr.Range.Font.Bold = fBold;
+            fPr.Range.Font.Color = fColor;
+            fPr.Range.Font.Name = fFntName;
+            fPr.Range.Text = fText;
+            fPr.Alignment = fAlign;
+        }
+
+        private void pageCfg(ref W.Document fDoc, float fLeft, 
+            float fRigth, float fTop, float fBottom)
+        {
+            fDoc.PageSetup.LeftMargin = 
+                fDoc.Application.CentimetersToPoints(fLeft);
+            fDoc.PageSetup.RightMargin = 
+                fDoc.Application.CentimetersToPoints(fRigth);
+            fDoc.PageSetup.TopMargin = 
+                fDoc.Application.CentimetersToPoints(fTop);
+            fDoc.PageSetup.BottomMargin = 
+                fDoc.Application.CentimetersToPoints(fBottom);
+        }
+
+        private void ParagraphDemo()
+        {
+            W.Application oWord;
+            W.Document oDoc;
+            W.Paragraph oPr;
+
+            oWord = new W.Application();
+            oDoc = oWord.Documents.Add();
+            oPr = oDoc.Paragraphs.Add();
+
+            CustomizedParagraph(ref oPr, "Новый текст", 1, W.WdColor.wdColorRed,
+                "Tahoma", W.WdParagraphAlignment.wdAlignParagraphCenter);
+            oPr.Range.InsertParagraphAfter();
+            CustomizedParagraph(ref oPr, "Следующий абзац", 1, W.WdColor.wdColorDarkGreen,
+                "Book Antiqua", W.WdParagraphAlignment.wdAlignParagraphLeft);
+            oPr.Range.InsertParagraphAfter();
+            CustomizedParagraph(ref oPr, "Ещё один абзац", fAlign: W.WdParagraphAlignment.wdAlignParagraphRight);
+            oPr.Range.InsertParagraphAfter();
+            CustomizedParagraph(ref oPr, "И ещё один", 1, fFntName: "Comic Sans MS");
+            oDoc.SaveAs2(Application.StartupPath + "\\Новый документ.docx");
+            oWord.Quit();
+        }
+
+        private void PageSetUpDemo()
+        {
+            try
+            {
+                W.Application oWord;
+                W.Document oDoc;
+
+                oWord = new W.Application();
+                oDoc = oWord.Documents.Add();
+
+                pageCfg(ref oDoc, 5f, 6f, 3f, 4f);
+
+                oDoc.SaveAs2(Application.StartupPath + "\\Новый документ.docx");
+                oWord.Quit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Произошла следующая ошибка: {ex.ToString()}", "ОШИБКА!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void TabStopsDemo()
+        {
+            W.Application oWord;
+            W.Document oDoc;
+            W.Paragraph oPr;
+
+            oWord = new W.Application();
+            oDoc = oWord.Documents.Add();
+            oPr = oDoc.Paragraphs.Add();
+
+            oPr.Range.Text = "Заведующий каф. \"УиЗИ\"\tЛ.А. Баранов";
+            oPr.TabStops.Add(oWord.CentimetersToPoints(16.5f),
+                W.WdTabAlignment.wdAlignTabList,
+                W.WdTabLeader.wdTabLeaderLines);
+
+            oDoc.SaveAs2(Application.StartupPath + "\\Новый документ.docx");
+            oWord.Quit();
+        }
+
+        private void ToFillGrid()
+        {
+            string tmpStr;
+            string[] Record;
+            int counter = 0;
+            StreamReader sr = new StreamReader(Application.StartupPath + "\\articles.csv");
+
+            dgMain.Rows.Clear();
+            dgMain.Columns.Clear();
+            while ((tmpStr = sr.ReadLine()) != null)
+            {
+                Record = tmpStr.Split(new char[] { ';' });
+                if (counter == 0)
+                {
+                    for (int i = 0; i < Record.Length; i++)
+                        dgMain.Columns.Add("col" + i, Record[i]);
+                }
+                else
+                {
+                    dgMain.Rows.Add();
+                    for (int i = 0; i < Record.Length; i++)
+                        dgMain[i, counter - 1].Value = Record[i];
+                }
+                counter++;
+            }
+
+            sr.Close();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            W.Application oWord;
+            W.Document oDoc;
+            W.Paragraph oPar;
+            W.Table oTab;
+            W.Cell oCell;
+            float[] colWdt = new float[] { 0.99f, 5.25f, 1.5f, 5.25f, 1.5f, 2.01f };
+
+            oWord = new W.Application();
+            oDoc = oWord.Documents.Add();
+            oPar = oDoc.Paragraphs.Add();
+
+            ToFillGrid();
+
+            oTab = oDoc.Tables.Add(oPar.Range, dgMain.RowCount, dgMain.ColumnCount);
+
+            oTab.Borders[W.WdBorderType.wdBorderTop].Visible = true;
+            oTab.Borders[W.WdBorderType.wdBorderBottom].Visible = true;
+            oTab.Borders[W.WdBorderType.wdBorderLeft].Visible = true;
+            oTab.Borders[W.WdBorderType.wdBorderRight].Visible = true;
+            oTab.Borders[W.WdBorderType.wdBorderVertical].Visible = true;
+            oTab.Borders[W.WdBorderType.wdBorderHorizontal].Visible = true;
+
+            for (int j = 0; j < dgMain.ColumnCount; j++)
+            {
+                oTab.Columns[j + 1].Width = oWord.CentimetersToPoints(colWdt[j]);
+                oCell = oTab.Cell(1, j + 1);
+                oCell.Range.Font.Size = 10;
+                oCell.Range.ParagraphFormat.Space1();
+                oCell.Range.Text = dgMain.Columns[j].HeaderText;
+            }
+
+            for (int i = 0; i < dgMain.RowCount; i++)
+                for (int j = 0; j < dgMain.ColumnCount; j++)
+                    if (dgMain[j, i].Value != null)
+                    {
+                        oCell = oTab.Cell(i + 2, j + 1);
+                        oCell.Range.Font.Size = 10;
+                        oCell.Range.ParagraphFormat.Space1();
+                        oCell.Range.Text = dgMain[j, i].Value.ToString();
+                    }
+
+            oDoc.SaveAs2(Application.StartupPath + "\\Список трудов.docx");
+            oWord.Quit();
+        }
+    }
+}
